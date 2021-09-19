db4ea998cadd58673fe06893710d2fcda094abc9
Thomas Sourianos <thomasourianos@live.no>
2021-09-19 19:21:20 +0200

    finished

--- src/components/PortfolioDisplay/PortfolioDisplay.js
+++ src/components/PortfolioDisplay/PortfolioDisplay.js
@@ -5,13 +5,14 @@ export default function PortfolioDisplay({title, desc, image}) {
     return (
         <>
             <div className="portfolio-container">
+                <div className="portfolio-container-right">
+                    <img src={image} alt="Project Image" />
+                </div>
                 <div className="portfolio-container-left">
                     <h3 className="portfolio-title">{title}</h3>
                     <p>{desc}</p>
                 </div>
-                <div className="portfolio-container-right">
-                    <img src={image} alt="Project Image" />
-                </div>
+
             </div>
         </>
     )
