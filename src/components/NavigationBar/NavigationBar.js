9b19e9fc3695ccc8c3270c7948bbb502a614fdc3
Thomas Sourianos <thomasourianos@live.no>
2021-09-19 16:47:22 +0200

    landing page done, refactor

---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,20 @@
+import {Link} from "react-router-dom";
+import logo from "../../assets/logoipsum-logo-15 1.png";
+import "./NavigationBar.css"
+
+export default function NavigationBar() {
+    return <>
+        <div className="logo">
+            <li><Link className="link" to="/"><img src={logo} alt="logo"/></Link>
+            </li>
+        </div>
+        <div className="links">
+            <li><Link className="link" to="/portfolio">Portfolio</Link>
+            </li>
+            <li className="header-link"><Link className="link" to="/">Github</Link>
+            </li>
+            <li className="header-link"><Link className="link" to="/">Contact</Link>
+            </li>
+        </div>
+    </>;
+}
