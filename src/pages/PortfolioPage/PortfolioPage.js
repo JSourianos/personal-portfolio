bec146574c56df7f2b468bd9bc853043b4952299
Thomas Sourianos <thomasourianos@live.no>
2021-09-21 16:36:30 +0200

    more imgs

--- src/pages/PortfolioPage/PortfolioPage.js
+++ src/pages/PortfolioPage/PortfolioPage.js
@@ -3,6 +3,9 @@ import "./PortfolioPage.css";
 
 //Assets
 import SchieEX from "../../assets/schieex.png";
+import EivindsCafe from "../../assets/eivindscafe2.png";
+import GreekBook from "../../assets/greekbook.png";
+import Eminus from "../../assets/eminus.png"
 
 
 //Components
@@ -22,7 +25,7 @@ const projectInfo = [
         title: "Eivinds Cafe, UX Interface Exam",
         desc: "This project was our exam delivery for “Grensesnittdesign”, which was a course in User Experience Design.\n" +
             "We used class-based React to design the frontend.",
-        image: SchieEX,
+        image: EivindsCafe,
         url: "https://grensesnitt-exam.netlify.app/"
     },
     {
@@ -31,21 +34,21 @@ const projectInfo = [
             "\n" +
             "We used React Native to create an extensive application which allowed the users to register parts of a lamp that has been broken, save this data and upload it to a main server. UX was a big part of the solution, as we had to adjust for the possibility that people might not know how to read. We solved this with tons of user testing, and designed the app in such a way that you wouldn’t need to know how to read to use it.",
         image: SchieEX,
-        url: "https://github.com/JSourianos/androidexam"
+        url: "https://github.com/Ammar1992-MK/pro201-smidigprosjekt"
     },
     {
         title: "GreekBook, Web Development Exam",
         desc: "This project was a solo exam, where I created a small application that allows you to register a user with Google, and enter the chat application. The app allows for chatting, with a group chat and private chat. The library that was used for chatting was Socket.io, and the entire app was written with React, using hooks.\n" +
             "\n" +
             "This app also has testing in it, where Jest was the technology used for writing tests.",
-        image: SchieEX,
+        image: GreekBook,
         url: "https://github.com/JSourianos/androidexam"
     },
     {
         title: "Eminus, eCommerce Website",
         desc: "Eminus is a Norwegian clothing business, who specializes in Vintage Clothing and resell. I function as the main developer of the website, and I also manage some of the business aspects, such as receving payments and organizing the designers, making sure that clothes being sold from the website reaches the customer.",
-        image: SchieEX,
-        url: "www.eminus.no"
+        image: Eminus,
+        url: "https://www.eminus.no"
     },
 ]
 
@@ -57,7 +60,8 @@ export default function PortfolioPage() {
                 <div className="display-container">
                     {projectInfo.map(project => {
                         return (
-                            <PortfolioDisplay href={project.url} title={project.title} desc={project.desc} image={project.image}/>
+                            <PortfolioDisplay href={project.url} title={project.title} desc={project.desc}
+                                              image={project.image}/>
                         )
                     })}
                 </div>
