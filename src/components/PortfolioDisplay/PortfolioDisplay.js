4a694f63779b1226488b8e6e84575c9294591762
Thomas Sourianos <thomasourianos@live.no>
2021-09-21 15:55:07 +0200

    new link for interface design

--- src/components/PortfolioDisplay/PortfolioDisplay.js
+++ src/components/PortfolioDisplay/PortfolioDisplay.js
@@ -1,19 +1,18 @@
 import * as React from "react";
 import "./PortfolioDisplay.css";
 
-export default function PortfolioDisplay({title, desc, image}) {
+export default function PortfolioDisplay({title, desc, image, href}) {
     return (
         <>
             <div className="portfolio-container">
                 <div className="portfolio-container-right">
-                    <a href="https://github.com/JSourianos/Grensesnittdesign-Interface-Design" target="_blank"><img
+                    <a href={href} target="_blank"><img
                         src={image} alt="Project Image"/></a>
                 </div>
                 <div className="portfolio-container-left">
                     <h3 className="portfolio-title">{title}</h3>
                     <p>{desc}</p>
                 </div>
-
             </div>
         </>
     )
